1fc33a8e653b21ca79351790603e5613f1be964f
dokkalemon <dottori.roberto@gmail.com>
2022-01-26 21:32:58 +0100

    create a show section

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('content')
+<section>
+    <h1 class="mt-5">Create a new Comic</h1>
+
+    <div class="row">
+        .col-md-8 offset-md-2
+    </div>
+</section>
+    
+@endsection
